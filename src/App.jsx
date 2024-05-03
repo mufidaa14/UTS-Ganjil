3e7e8d70d5286b9159950842a443f3c054650151
mufidaa14 <mufidajeje14@gmail.com>
2024-05-03 09:58:30 +0700

    penambahan tab dan navbar icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View,} from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Beranda from './screens/Beranda';
 
-const App = () => {
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const Bar = () => {
+  return (
+    <Tab.Navigator screenOptions={{tabBarActiveTintColor: 'orange'}}>
+      <Tab.Screen 
+      name="Beranda" 
+      component={Beranda}
+      options={{
+         tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+  />
+  </Tab.Navigator>
+  );
+};
+
+/**pindah posisii */
+const App = ()=> {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Beranda" component={Malin_Kundang} />
+        <Stack.Screen name="Danau Toba" component={Danau_Toba} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+const Malin_Kundang = ({navigation}) => {
   return (
     <View>
-      <Text>hello</Text>
+      <Text style={styles.Malin_Kundang}>Malin Kundang</Text>
+      <Button 
+      title='danau toba'
+      onPress={()=>navigation.navigate('Danau Toba')}/>
     </View>
-  )
-}
+  );
+};
 
-export default App
+const Danau_Toba = () => {
+  return (
+    <View>
+      <Text style={styles.Danau_Toba}>Danau toba</Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default App;
+
+const styles = StyleSheet.create({
+  Malin_Kundang: {
+    fontSize: 100,
+  }
+});
--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,21 +1,14 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import App from '../App';
-const Stack = createNativeStackNavigator();
 
 const Beranda = () => {
-    return (
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={App} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      );
+  return (
+    <View>
+      <Text>Sayaaa</Text>
+    </View>
+  );
 };
 
-export default Beranda
+export default Beranda;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
